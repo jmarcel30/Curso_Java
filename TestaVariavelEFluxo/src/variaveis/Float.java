035ad9219b9e4ffd16d53cbb13d68193f1d6d78c
José Marcel <josemarcel30@gmail.com>
2023-08-14 19:25:54 -0300

    Variável double

---
+++ TestaVariavelEFluxo/src/variaveis/Float.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class Float {
+	public static void main(String[] args) {
+	double horas = 13;
+	
+	System.out.println("Agora são exatamente: " + horas  +  "Horas");
+	
+	double nota1 = 5.6;
+	double nota2 = 7.3;
+	double nota3 = 6.9;
+	double nota4 = 9;
+	
+	double total = (nota1 + nota2 + nota3 + nota4) / 4;
+	
+	System.out.println("Sua medi final é:" + total);
+}
+}
