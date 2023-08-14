441f49f1ca8fe003aa757f96c82d721e4903d1fd
José Marcel <josemarcel30@gmail.com>
2023-08-14 18:48:52 -0300

    Variável int + Concatenação

--- HelloWord/src/cursoJava/Helloword.java
+++ HelloWord/src/cursoJava/Helloword.java
@@ -2,7 +2,7 @@ package cursoJava;
 
 public class Helloword {
 	public static void main(String[] args) {
-		System.out.println("Ola mundo");
+		System.out.println("Hello Word");
 	}
 
 }
---
+++ TestaVariavelEFluxo/src/variaveis/Inteiro.java
@@ -0,0 +1,12 @@
+package variaveis;
+
+public class Inteiro {
+	public static void main(String[] args) {
+		int idade1 = 12;
+		int idade2 = 25;
+		int idade3 = 56;
+	
+		System.out.println("A idade 1 é:" + idade1);
+	
+	}
+}
