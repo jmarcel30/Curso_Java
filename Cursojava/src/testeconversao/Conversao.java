b9239a2586c85938995bb8b57b50e420f7833924
José Marcel <josemarcel30@gmail.com>
2023-08-16 23:22:00 -0300

    Cast e tipos de variáveis numéricas

---
+++ Cursojava/src/testeconversao/Conversao.java
@@ -0,0 +1,24 @@
+package testeconversao;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+		
+		double salario = 2500.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		
+		int valor2 = 20000000;
+		long numeroGrande = 1231321321321321L;
+		short numeroPequeno = 2023;
+		byte b = 127;
+		float pontoFlutuante = (float)3.45;
+	    
+				
+		
+	
+
+	}
+
+}
