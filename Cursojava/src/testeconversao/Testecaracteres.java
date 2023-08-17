6a20e574b2e44669ebe7839aabca109b12dccb01
José Marcel <josemarcel30@gmail.com>
2023-08-16 23:44:20 -0300

    Caracteres e String

---
+++ Cursojava/src/testeconversao/Testecaracteres.java
@@ -0,0 +1,32 @@
+package testeconversao;
+
+public class Testecaracteres {
+
+	public static void main(String[] args) {
+		char letra  =  'Q';
+		
+		System.out.println(letra);
+		
+		char valor = 56;
+		System.out.println(valor);
+		
+		 valor = (char) (valor + 1);
+		System.out.println(valor); 
+		
+		String mensagem = "Seja bem vindo";
+		
+		System.out.println(mensagem);
+		
+		String mensagem2 = "";
+		
+		mensagem2 = "ola mundo";
+		
+		System.out.println(mensagem2);
+		
+		System.out.println(mensagem  +  mensagem2);
+		
+		
+
+	}
+
+}
---
+++ Cursojava/src/variaveis/Testavariaveis.java
@@ -0,0 +1,17 @@
+package variaveis;
+
+public class Testavariaveis {
+
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		
+		segundo = primeiro;
+		
+		primeiro = 10;
+		
+		System.out.println(segundo);
+
+	}
+
+}
