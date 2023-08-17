c63b608ab53e15ba6f7313822ba21b23797cc161
José Marcel <josemarcel30@gmail.com>
2023-08-17 11:07:30 -0300

    Aula de  if e else  em java

---
+++ Cursojava/src/variaveis/TestandoVariaveis.java
@@ -0,0 +1,17 @@
+package variaveis;
+
+public class TestandoVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais.");
+		int idade = 20;
+		
+		if(idade >= 18) {
+			System.out.println("Sua idade é " + idade + " anos você Pode comorar a bebida");
+		} else {
+			System.out.println("Sua idade é "+idade+" Você não pode comprar bebidas ");
+		}
+
+	}
+
+}
