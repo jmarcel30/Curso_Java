8e87bc732e32fbbd97dd00133f94f4771b08d718
José Marcel <josemarcel30@gmail.com>
2023-09-22 13:33:48 -0300

    Exercício de for

---
+++ ArrayEx.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ArrayEx {
+    public static void main(String[] args) {
+        int[] num={10,50,30,60,80};
+
+        Arrays.sort(num);
+        
+
+        for(int n:num){
+            System.out.println(n);
+            
+        }
+    }
+}
