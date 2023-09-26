e01a028b5466e427f8d1e0a29f7c3ed1341fb0d0
José Marcel <josemarcel30@gmail.com>
2023-09-26 11:14:30 -0300

    Estudando classes

---
+++ Classes/Classe.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class Classe {
+    public static void main(String[] args) {
+        
+        Jogador j1 = new Jogador(1);
+        Jogador j2 = new Jogador(2);
+        Jogador j3 = new Jogador(3);
+    }
+}
---
+++ Classes/Jogador.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public class Jogador {
+    public Jogador(int n){
+        System.out.printf("%nJogador numero %d Criado%n",n);
+    }
+
+}
